fac522a3ada224530a57120ac0862c92a7f8c7a9
keiry11 <keiryalexandraquijadaperez@gmail.com>
2024-10-06 02:22:28 -0600

    project started

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+// captura formulario 
+const form = document.getElementById('crudForm');
+const dataTable = document.getElementById('dataTable');
+
+// consumir API READ
+
+const getData = async () => {
+    try {
+        const response = await axios.get(apiUrl);
+        renderTable(response.data);
+    } catch (error) {
+        console.error('Error al obtener los datos', error);
+    }
+}
+
+const renderTable = (data) => {
+    console.log(data);
+    dataTable.innerHTML = '';
+    data.forEach(item => {
+        const row = `
+<tr class="border-b"></tr>
+        <td class="py-2 px-4">${item.name}</td>
+        <td class="py-2 px-4">${item.lastName}</td>
+        <td class="py-2 px-4">${item.phone}</td>
+        <td class="py-2 px-4">${item.direction}</td>
+        <td class="py-2 px-4">${item.reasons}</td>
+        <td class="py-2 px-4"><img class="w-16 h-16 object-cover rounded" src="${item.img}" alt=""></td>
+       
+        </tr>`;
+        dataTable.insertAdjacentHTML('beforeend', row);
+    });
+}
+
+getData();
