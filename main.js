18cc05408fdaabd2b482dfcb9fdcb8cad56ed23c
keiry11 <keiryalexandraquijadaperez@gmail.com>
2024-10-12 22:16:05 -0600

    :lipstick: add new changes

--- main.js
+++ main.js
@@ -34,3 +34,62 @@ const renderTable = (data) => {
 }
 
 getData();
+
+
+// Selección de elementos
+const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+const mobileMenu = document.getElementById('mobile-menu');
+const userMenuButton = document.getElementById('user-menu-button');
+const userMenu = userMenuButton.nextElementSibling;
+
+// Estado del menú
+let menuMovilAbierto = false;
+let menuUsuarioAbierto = false;
+
+// Función para mostrar/ocultar el menú móvil
+function toggleMobileMenu() {
+    menuMovilAbierto = !menuMovilAbierto;
+    if (menuMovilAbierto) {
+        mobileMenu.classList.remove('hidden');
+        mobileMenuButton.setAttribute('aria-expanded', 'true');
+    } else {
+        mobileMenu.classList.add('hidden');
+        mobileMenuButton.setAttribute('aria-expanded', 'false');
+    }
+}
+
+// Función para mostrar/ocultar el menú del usuario
+function toggleUserMenu() {
+    menuUsuarioAbierto = !menuUsuarioAbierto;
+    if (menuUsuarioAbierto) {
+        userMenu.classList.add('transform', 'opacity-100', 'scale-100');
+        userMenu.classList.remove('opacity-0', 'scale-95');
+        userMenu.setAttribute('aria-expanded', 'true');
+    } else {
+        userMenu.classList.add('opacity-0', 'scale-95');
+        userMenu.classList.remove('transform', 'opacity-100', 'scale-100');
+        userMenu.setAttribute('aria-expanded', 'false');
+    }
+}
+
+// Cerrar el menú del usuario si se hace clic fuera de él
+function cerrarMenusSiFueraClick(event) {
+    if (!userMenuButton.contains(event.target) && !userMenu.contains(event.target)) {
+        menuUsuarioAbierto = false;
+        userMenu.classList.add('opacity-0', 'scale-95');
+        userMenu.classList.remove('transform', 'opacity-100', 'scale-100');
+        userMenu.setAttribute('aria-expanded', 'false');
+    }
+    if (!mobileMenuButton.contains(event.target) && !mobileMenu.contains(event.target)) {
+        menuMovilAbierto = false;
+        mobileMenu.classList.add('hidden');
+        mobileMenuButton.setAttribute('aria-expanded', 'false');
+    }
+}
+
+// Eventos para alternar los menús
+mobileMenuButton.addEventListener('click', toggleMobileMenu);
+userMenuButton.addEventListener('click', toggleUserMenu);
+
+// Evento para cerrar los menús si se hace clic fuera de ellos
+document.addEventListener('click', cerrarMenusSiFueraClick);
